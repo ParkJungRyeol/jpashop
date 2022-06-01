1faad18bcf798054ff8a58fe47f559d8e9bc0b08
pjr <jungryeolpark92gmail.com>
2022-06-01 21:30:52 +0900

    스프링부트와 JPA활용1 학습

---
+++ src/main/java/jpabook/jpashop/Hello.java
@@ -0,0 +1,12 @@
+package jpabook.jpashop;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@RequiredArgsConstructor
+public class Hello {
+    private String data;
+}
